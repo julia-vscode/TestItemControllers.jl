c4331fa02ac80a6f7e2ab8be2335747f3f81a794
David Anthoff <anthoff@berkeley.edu>
2025-06-25 17:15:06 +0200

    Squashed 'packages/LoweredCodeUtils/' changes from 934fe49..449fafe
    449fafe add docs/.gitattributes
    bcb6f63 update to JuliaInterpreter 0.10 (#127)
    498e2fb version 3.2.2
    a162364 update to JuliaInterpreter 0.9.46 (#128)
    c3ee610 Restore `trackedheads` (#126)
    c0f289a version 3.2.0
    cea0537 adjustments to v1.12 (#124)
    c7d1b30 Bump codecov/codecov-action from 4 to 5 (#122)
    
    git-subtree-dir: packages/LoweredCodeUtils
    git-subtree-split: 449fafed49c0e2834e48a1c30f78988e9d9ad702

--- src/LoweredCodeUtils.jl
+++ src/LoweredCodeUtils.jl
@@ -10,10 +10,10 @@ module LoweredCodeUtils
 # the VS Code extension integration.
 
 using JuliaInterpreter
-using JuliaInterpreter: SSAValue, SlotNumber, Frame
-using JuliaInterpreter: @lookup, moduleof, pc_expr, step_expr!, is_global_ref, is_quotenode_egal, whichtt,
-                        next_until!, finish_and_return!, get_return, nstatements, codelocation, linetable,
-                        is_return, lookup_return
+using JuliaInterpreter: SSAValue, SlotNumber, Frame, Interpreter, RecursiveInterpreter
+using JuliaInterpreter: codelocation, is_global_ref, is_global_ref_egal, is_quotenode_egal, is_return,
+                        lookup, lookup_return, linetable, moduleof, next_until!, nstatements, pc_expr,
+                        step_expr!, whichtt
 
 include("packagedef.jl")
 
