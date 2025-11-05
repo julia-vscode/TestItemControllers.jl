05bef6c7ad910b08d75f3d2adc88557cb11f4ce3
David Anthoff <anthoff@berkeley.edu>
2025-11-05 15:50:20 -0800

    Fix some imports

--- testprocess/TestItemServer/src/pkg_imports.jl
+++ testprocess/TestItemServer/src/pkg_imports.jl
@@ -42,11 +42,13 @@ end
 
 module LoweredCodeUtils
     @static if VERSION >= v"1.10.0"
+        using ..CodeTracking: MethodInfoKey
+
         using ..JuliaInterpreter
         using ..JuliaInterpreter: SSAValue, SlotNumber, Frame, Interpreter, RecursiveInterpreter
         using ..JuliaInterpreter: codelocation, is_global_ref, is_global_ref_egal, is_quotenode_egal, is_return,
-            lookup, lookup_return, linetable, moduleof, next_until!, nstatements, pc_expr,
-            step_expr!, whichtt
+                        lookup, lookup_return, linetable, moduleof, next_until!, nstatements, pc_expr,
+                        step_expr!, whichtt, extract_method_table
         using ..Compiler: Compiler as CC
 
         include("../../../packages/LoweredCodeUtils/src/packagedef.jl")
@@ -70,21 +72,40 @@ module LoweredCodeUtils
 end
 
 module Revise
-    using ..OrderedCollections
-    using ..LoweredCodeUtils
-    using ..CodeTracking
-    using ..JuliaInterpreter
-    using ..CodeTracking: PkgFiles, basedir, srcfiles, line_is_decl, basepath
-    using ..JuliaInterpreter: whichtt, is_doc_expr, step_expr!, finish_and_return!, get_return,
-        @lookup, moduleof, scopeof, pc_expr, is_quotenode_egal,
-        linetable, codelocs, LineTypes, isassign, isidentical
-    using ..LoweredCodeUtils: next_or_nothing!, trackedheads, callee_matches
-
     @static if VERSION >= v"1.10.0"
+        using ..OrderedCollections, ..CodeTracking, ..JuliaInterpreter, ..LoweredCodeUtils
+
+        using ...CodeTracking: PkgFiles, basedir, srcfiles, basepath, MethodInfoKey
+        using ...JuliaInterpreter: Compiled, Frame, Interpreter, LineTypes, RecursiveInterpreter
+        using ...JuliaInterpreter: codelocs, finish_and_return!, get_return, is_doc_expr, isassign,
+                        isidentical, is_quotenode_egal, linetable, lookup, moduleof,
+                        pc_expr, scopeof, step_expr!
+        using ...LoweredCodeUtils: next_or_nothing!, callee_matches
+
         include("../../../packages/Revise/src/packagedef.jl")
     elseif VERSION >= v"1.6.0"
+        using ..OrderedCollections
+        using ..LoweredCodeUtils
+        using ..CodeTracking
+        using ..JuliaInterpreter
+        using ..CodeTracking: PkgFiles, basedir, srcfiles, line_is_decl, basepath
+        using ..JuliaInterpreter: whichtt, is_doc_expr, step_expr!, finish_and_return!, get_return,
+            @lookup, moduleof, scopeof, pc_expr, is_quotenode_egal,
+            linetable, codelocs, LineTypes, isassign, isidentical
+        using ..LoweredCodeUtils: next_or_nothing!, trackedheads, callee_matches
+
         include("../../../packages-old/v1.9/Revise/src/packagedef.jl")
     else
+        using ..OrderedCollections
+        using ..LoweredCodeUtils
+        using ..CodeTracking
+        using ..JuliaInterpreter
+        using ..CodeTracking: PkgFiles, basedir, srcfiles, line_is_decl, basepath
+        using ..JuliaInterpreter: whichtt, is_doc_expr, step_expr!, finish_and_return!, get_return,
+            @lookup, moduleof, scopeof, pc_expr, is_quotenode_egal,
+            linetable, codelocs, LineTypes, isassign, isidentical
+        using ..LoweredCodeUtils: next_or_nothing!, trackedheads, callee_matches
+
         include("../../../packages-old/v1.5/Revise/src/packagedef.jl")
     end
 end
